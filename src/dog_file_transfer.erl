3bacc47eea253252642163ebc472a5360240be32
Drew Gulino <dgulino@relaypro.com>
2022-06-13 12:50:55 +0000

    added exceptions to ensure mq ack, parsing multi-command ansible responses properly

--- src/dog_file_transfer.erl
+++ src/dog_file_transfer.erl
@@ -43,120 +43,157 @@ subscriber_loop(_RoutingKey, _CType, Payload, State) ->
     lager:debug("Command: ~p",[Command]),
     case Command of
         send_file ->
-            lager:debug("send_file"),
-            FilenameClean = filelib:safe_relative_path(string:trim(Filename,leading,"/"),[]),
-            FilePath = ?SANDBOX_FILE_ROOT ++ FilenameClean,
-            case FilenameClean of
-                unsafe ->
-                    lager:debug("Unsafe FilePath"),
-                    {reply, <<"text/json">>, jsx:encode(file_bad), State};
-                _ ->
-                    FileTotalBlocks = proplists:get_value(total_blocks, Message),
-                    FileCurrentBlock = proplists:get_value(current_block, Message),
-                    MaxBlockSizeBytes = proplists:get_value(max_block_size_bytes, Message),
-                    FileBlock = maps:get(file_block, UserData),
-                    lager:debug("FilePath: ~p",[FilePath]),
-                    lager:debug("Filename: ~p, MaxBlockSizeBytes: ~p, Block ~p of ~p",[Filename,MaxBlockSizeBytes,FileCurrentBlock,FileTotalBlocks]),
-                    %{ok,IoDevice} = file:open(FilePath,[write,binary,read_ahead,raw]),
-                    {ok,IoDevice} = case FileCurrentBlock of
-                        1 ->
-                            filelib:ensure_dir(filename:dirname(FilePath) ++ "/"),
-                            file:open(FilePath,[write,raw]);
-                        _ ->
-                            %file:open(FilePath,[append,raw])
-                            file:open(FilePath,[write,read,raw])
-                    end,
-                    case FileCurrentBlock of
-                        1 when FileTotalBlocks =:= 1 ->
-                            file:pwrite(IoDevice,0,FileBlock),
-                            file:close(IoDevice),
-                            {ack,State};
+            try
+                lager:debug("send_file"),
+                FilenameClean = filelib:safe_relative_path(string:trim(Filename,leading,"/"),[]),
+                FilePath = ?SANDBOX_FILE_ROOT ++ FilenameClean,
+                case FilenameClean of
+                    unsafe ->
+                        lager:debug("Unsafe FilePath"),
+                        {reply, <<"text/json">>, jsx:encode(file_bad), State};
+                    _ ->
+                        FileTotalBlocks = proplists:get_value(total_blocks, Message),
+                        FileCurrentBlock = proplists:get_value(current_block, Message),
+                        MaxBlockSizeBytes = proplists:get_value(max_block_size_bytes, Message),
+                        FileBlock = maps:get(file_block, UserData),
+                        lager:debug("FilePath: ~p",[FilePath]),
+                        lager:debug("Filename: ~p, MaxBlockSizeBytes: ~p, Block ~p of ~p",[Filename,MaxBlockSizeBytes,FileCurrentBlock,FileTotalBlocks]),
+                        %{ok,IoDevice} = file:open(FilePath,[write,binary,read_ahead,raw]),
+                        {ok,IoDevice} = case FileCurrentBlock of
+                                            1 ->
+                                                filelib:ensure_dir(filename:dirname(FilePath) ++ "/"),
+                                                file:open(FilePath,[write,raw]);
+                                            _ ->
+                                                %file:open(FilePath,[append,raw])
+                                                file:open(FilePath,[write,read,raw])
+                                        end,
+                        case FileCurrentBlock of
+                            1 when FileTotalBlocks =:= 1 ->
+                                file:pwrite(IoDevice,0,FileBlock),
+                                file:close(IoDevice),
+                                {ack,State};
                             %{reply, <<"text/json">>, jsx:encode(block_ok), State};
-                        1 when FileTotalBlocks > 1 ->
-                            file:pwrite(IoDevice,0,FileBlock),
+                            1 when FileTotalBlocks > 1 ->
+                                file:pwrite(IoDevice,0,FileBlock),
+                                %{reply, <<"text/json">>, jsx:encode(block_ok), State};
+                                {ack,State};
+                            N when N >=  FileTotalBlocks ->
+                                %file:write(IoDevice,FileBlock),
+                                StartByte = (FileCurrentBlock - 1) * MaxBlockSizeBytes,
+                                lager:debug("StartByte: ~p",[StartByte]),
+                                file:pwrite(IoDevice,StartByte,FileBlock),
+                                file:close(IoDevice),
+                                {ack,State};
                             %{reply, <<"text/json">>, jsx:encode(block_ok), State};
-                            {ack,State};
-                        N when N >=  FileTotalBlocks ->
-                            %file:write(IoDevice,FileBlock),
-                            StartByte = (FileCurrentBlock - 1) * MaxBlockSizeBytes,
-                            lager:debug("StartByte: ~p",[StartByte]),
-                            file:pwrite(IoDevice,StartByte,FileBlock),
-                            file:close(IoDevice),
-                            {ack,State};
-                            %{reply, <<"text/json">>, jsx:encode(block_ok), State};
-                        _ ->
-                            %file:write(IoDevice,FileBlock),
-                            StartByte = (FileCurrentBlock - 1) * MaxBlockSizeBytes,
-                            lager:debug("StartByte: ~p",[StartByte]),
-                            file:pwrite(IoDevice,StartByte,FileBlock),
-                            {ack,State}
-                            %{reply, <<"text/json">>, jsx:encode(file_ok), State}
-                end    
+                            _ ->
+                                %file:write(IoDevice,FileBlock),
+                                StartByte = (FileCurrentBlock - 1) * MaxBlockSizeBytes,
+                                lager:debug("StartByte: ~p",[StartByte]),
+                                file:pwrite(IoDevice,StartByte,FileBlock),
+                                {ack,State}
+                                %{reply, <<"text/json">>, jsx:encode(file_ok), State}
+                        end 
+                end                                                                           
+            of
+                {ack, NewState} -> {ack,NewState}
+            catch
+                Exception:Reason:Stacktrace -> 
+                    lager:error("~p, ~p, ~p",[Exception, Reason, Stacktrace]),
+                    {ack, Reason}
             end;
         delete_file ->
-            FilenameClean = filelib:safe_relative_path(string:trim(Filename,leading,"/"),[]),
-            FilePath = ?SANDBOX_FILE_ROOT ++ FilenameClean,
-            case FilenameClean of
-                unsafe ->
-                    lager:debug("Unsafe FilePath"),
-                    {reply, <<"text/json">>, jsx:encode(file_bad), State};
-                _ ->
-                    lager:debug("FilePath: ~p",[FilePath]),
-                    case filelib:is_dir(FilePath) of
-                        true ->
-                            case file:del_dir(FilePath) of
-                                         {error,Reason} ->
-                                            {reply, <<"text/json">>, jsx:encode([{error,Reason}])};
-                                         ok ->
-                                            %{ack,State};
-                                            {reply, <<"text/json">>, jsx:encode([ok]), State}
-                                     end;
-                        false ->
-                            case file:delete(FilePath) of
-                                         {error,Reason} ->
-                                            {reply, <<"text/json">>, jsx:encode([{error,Reason}])};
-                                         ok ->
-                                            %{ack,State};
-                                            {reply, <<"text/json">>, jsx:encode([ok]), State}
-                                     end
-                    end
+            try
+                FilenameClean = filelib:safe_relative_path(string:trim(Filename,leading,"/"),[]),
+                FilePath = ?SANDBOX_FILE_ROOT ++ FilenameClean,
+                case FilenameClean of
+                    unsafe ->
+                        lager:debug("Unsafe FilePath"),
+                        {reply, <<"text/json">>, jsx:encode(file_bad), State};
+                    _ ->
+                        lager:debug("FilePath: ~p",[FilePath]),
+                        case filelib:is_dir(FilePath) of
+                            true ->
+                                case file:del_dir(FilePath) of
+                                             {error,Error} ->
+                                                {reply, <<"text/json">>, jsx:encode([{error,Error}])};
+                                             ok ->
+                                                %{ack,State};
+                                                {reply, <<"text/json">>, jsx:encode([ok]), State}
+                                         end;
+                            false ->
+                                case file:delete(FilePath) of
+                                             {error,Error} ->
+                                                {reply, <<"text/json">>, jsx:encode([{error,Error}])};
+                                             ok ->
+                                                %{ack,State};
+                                                {reply, <<"text/json">>, jsx:encode([ok]), State}
+                                         end
+                        end
+                end
+            of
+                Reply -> Reply
+            catch
+                Exception:Reason:Stacktrace -> 
+                    lager:error("~p, ~p, ~p",[Exception, Reason, Stacktrace]),
+                    {reply, <<"text/json">>, jsx:encode([Reason]), State}
             end;
         fetch_file ->
-            lager:debug("Filename: ~p",[Filename]),
-            case file:read_file(Filename) of
-                {ok,Bytes} ->
-                    {reply, <<"application/octet-stream">>, Bytes, State};
-                {error, Reason} ->
-                    {reply, <<"text/json">>, jsx:encode([{error, Reason}]), State}
+            try
+                lager:debug("Filename: ~p",[Filename]),
+                case file:read_file(Filename) of
+                    {ok,Bytes} ->
+                        {reply, <<"application/octet-stream">>, Bytes, State};
+                    {error, Error} ->
+                        {reply, <<"text/json">>, jsx:encode([{error, Error}]), State}
+                end
+            of
+                Reply -> Reply
+            catch
+                Exception:Reason:Stacktrace -> 
+                    lager:error("~p, ~p, ~p",[Exception, Reason, Stacktrace]),
+                    {reply, <<"text/json">>, jsx:encode([Reason]), State}
             end;
         execute_command ->
-            ExecuteCommandBase64 = proplists:get_value(execute_command, Message),
-            ExecuteCommandRaw = base64:decode(ExecuteCommandBase64),
-            lager:debug("ExecuteCommandRaw: ~p",[ExecuteCommandRaw]),
-            UseShell = proplists:get_value(use_shell, Message, false),
-            RunAsUser = proplists:get_value(user, Message, "dog"),
-            ExecuteCommand = case UseShell of                                          
-                                 true ->                                                 
-                                     ExecuteCommandRaw;      
-                                 false ->                                                
-                                     string:split(ExecuteCommandRaw," ")
-                            end,                                                    
-            Result = exec:run(ExecuteCommand, [pty,sync, stdout, stderr, {user, RunAsUser}]),
-            lager:debug("Result: ~p",[Result]),
-            case Result of
-                {ok,[{stdout,StdOut}]} ->
-                    case length(StdOut) >= 1 of
-                        true ->
-                            {reply, <<"text/json">>, jsx:encode([{ok, lists:last(StdOut)}]), State};
-                        false ->
-                            {reply, <<"text/json">>, jsx:encode([{ok, StdOut}]), State}
-                    end;
-                {ok,[]} ->
-                    {reply, <<"text/json">>, jsx:encode([{ok, []}]), State};
-                {error,[{exit_status,_ExitStatus},{stderr,StdErr}]} ->
-                    {reply, <<"text/json">>, jsx:encode([{error, StdErr}]), State};
-                UnknownResponse ->
-                    lager:debug("UnknownResponse: ~p",[UnknownResponse])
+            try
+                ExecuteCommandBase64 = proplists:get_value(execute_command, Message),
+                ExecuteCommandRaw = base64:decode(ExecuteCommandBase64),
+                lager:debug("ExecuteCommandRaw: ~p",[ExecuteCommandRaw]),
+                UseShell = proplists:get_value(use_shell, Message, false),
+                RunAsUser = proplists:get_value(user, Message, "dog"),
+                ExecuteCommand = case UseShell of                                          
+                                     true ->                                                 
+                                         ExecuteCommandRaw;      
+                                     false ->                                                
+                                         string:split(ExecuteCommandRaw," ")
+                                end,                                                    
+                Result = exec:run(ExecuteCommand, [sync, stdout, stderr, {user, RunAsUser}]),
+                lager:debug("Result: ~p",[Result]),
+                case Result of
+                    {ok,[{stdout,StdOut}]} ->
+                        case length(StdOut) > 1 of
+                            true ->
+                                ParsedStdOut = [dog_common:binary_join(tl(StdOut),<<"">>)],
+                                lager:debug("ParsedStdOut: ~p",[ParsedStdOut]),
+                                {reply, <<"text/json">>, jsx:encode([{ok, ParsedStdOut}]), State};
+                            false ->
+                                {reply, <<"text/json">>, jsx:encode([{ok, StdOut}]), State}
+                        end;
+                    {ok,[]} ->
+                        {reply, <<"text/json">>, jsx:encode([{ok, []}]), State};
+                    {error,[{exit_status,_ExitStatus},{stdout,StdOut}]} ->
+                        {reply, <<"text/json">>, jsx:encode([{error, StdOut}]), State};
+                    {error,[{exit_status,_ExitStatus},{stderr,StdErr}]} ->
+                        {reply, <<"text/json">>, jsx:encode([{error, StdErr}]), State};
+                    UnknownResponse ->
+                        lager:debug("UnknownResponse: ~p",[UnknownResponse]),
+                        {reply, <<"text/json">>, {error, [UnknownResponse]}, State}
+                end
+            of
+                Reply -> Reply
+            catch
+                Exception:Reason:Stacktrace -> 
+                    lager:error("~p, ~p, ~p",[Exception, Reason, Stacktrace]),
+                    {reply, <<"text/json">>, jsx:encode([{error,Reason}]), State}
             end;
         _ ->
             lager:error("Unknown command: ~p",[Command]),
