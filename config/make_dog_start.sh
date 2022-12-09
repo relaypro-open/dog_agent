f48b8255b90c132d3ab5bd5d824bac18c00c4a82
Drew Gulino <dgulino@relaypro.com>
2022-12-09 17:12:38 -0500

    added 'safe mode' to stop/stop script generation

--- config/make_dog_start.sh
+++ config/make_dog_start.sh
@@ -1,6 +1,10 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
 echo "#!/bin/bash
 export BINDIR=/opt/dog/erts-"${ERTS_VERSION}"/bin
 export HOME=/opt/dog
-export HOSTKEY=$(sha1sum /var/consul/data/pki/certs/server.crt | cut -d' ' -f1)
+export HOSTKEY=
 
 /opt/dog/erts-"${ERTS_VERSION}"/bin/erlexec -boot /opt/dog/releases/${DOG_ENV}-${UPSTREAM_BUILD_ID}/start -mode embedded -config /opt/dog/releases/${DOG_ENV}-${UPSTREAM_BUILD_ID}/sys.config -noinput -start_epmd false" > dog_start.sh
--- config/make_dog_stop.sh
+++ config/make_dog_stop.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
 echo "#!/bin/bash
 /bin/kill -HUP \${cat /run/dog/dog.pid}
 rm -f /run/dog/dog.pid" > dog_stop.sh
