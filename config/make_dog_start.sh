d9d726057dffab7bc3ba5ae037d1b3a3dae45f7e
Drew Gulino <1379399+dgulinobw@users.noreply.github.com>
2023-11-03 14:48:09 -0400

    no longer using UPSTREAM_BUILD_ID in new Pipeline matrix build

--- config/make_dog_start.sh
+++ config/make_dog_start.sh
@@ -7,4 +7,4 @@ export BINDIR=/opt/dog/erts-"${erts_version}"/bin
 export HOME=/opt/dog
 export HOSTKEY=
 
-/opt/dog/erts-"${erts_version}"/bin/erlexec -boot /opt/dog/releases/${dog_env}-${UPSTREAM_BUILD_ID}/start -mode embedded -config /opt/dog/releases/${dog_env}-${UPSTREAM_BUILD_ID}/sys.config -noinput -start_epmd false" > dog_start.sh
+/opt/dog/erts-"${erts_version}"/bin/erlexec -boot /opt/dog/releases/${dog_env}-${BUILD_ID}/start -mode embedded -config /opt/dog/releases/${dog_env}-${BUILD_ID}/sys.config -noinput -start_epmd false" > dog_start.sh
